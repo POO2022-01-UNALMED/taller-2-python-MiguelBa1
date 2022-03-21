f7d3aa9f90abfbb945679e9950deac2c126be6ac
Miguel Barrera <59462086+MiguelBa1@users.noreply.github.com>
2022-03-21 11:27:21 -0500

    prueba

---
+++ main.py
@@ -0,0 +1,46 @@
+class Asiento:
+    def __init__(self, color, precio, registro):
+        self.color = color
+        self.precio = precio
+        self.registro = registro
+
+    def cambiarColor(self, color):
+        if color in ["rojo", "amarillo", "verde", "negro", "blanco"]:
+            self.color = color
+
+class Motor:
+    def __init__(self, numeroCilindros, tipo, registro):
+        self.numeroCilindros = numeroCilindros
+        self.tipo = tipo
+        self.registro = registro
+
+    def cambiarRegistro(self, registro):
+        self.registro = registro
+        
+    def asignarTipo(self, tipo):
+        if tipo in ["electrico", "gasolina"]:
+            self.tipo = tipo
+
+class Auto:
+    cantidadCreados = 0
+    def __init__(self, modelo, precio, asientos, marca, motor, registro):
+        self.modelo = modelo
+        self.precio = precio
+        self.asientos = asientos
+        self.marca = marca
+        self.motor = motor
+        self.registro = registro
+    
+    def cantidadAsientos(self):
+        return len(self.asientos)
+
+    def verficarIntegridad(self):
+        valido = True
+        for asiento in self.asientos:
+            if asiento.registro != self.resgistro:
+                valido = False
+
+        if self.motor.registro != self.registro:
+            valido = False
+        
+        return "Auto original" if valido else "Las piezas no son originales" 
